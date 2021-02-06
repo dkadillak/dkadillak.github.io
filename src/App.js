0edadb7725fec17d860217a4aa50dbb7220399d6
dkadillak <devin.kadillaka@gmail.com>
2021-02-06 12:43:05 -0700

    messin' with material ui, thinking of ux design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
+import { Button } from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
         <h1>HELLO WORLD</h1>
-      </header>
+        <Button fullWidth="true" size="large" color="secondary" variant="contained">this is a button</Button>
     </div>
   );
 }
