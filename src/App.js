676f5d8fee11550af985dad9de2d55c638fc1d68
dkadillak <devin.kadillaka@gmail.com>
2021-02-07 13:33:03 -0700

    some structure put in, mock header in

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
-import { Button } from '@material-ui/core';
+import React, { Component } from 'react';
+import { CssBaseline, Grid, Paper } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import MenuBar from './components/MenuBar/MenuBar';
+import { makeStyles } from '@material-ui/core/styles';
+import Landing from './assets/images/background/landing.jpg';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: "100vh",
+    backgroundImage: `url(${Landing})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat"
+  },
+}));
+
+/*
+
+      <CssBaseline/>
+      <Grid container direction="column">
+        <Grid item xs={12}>
+          <MenuBar></MenuBar>
+        </Grid>
+        <Grid item container>
+          <Grid item xs={0} sm={2}/>
+          <Grid item xs={12} sm={8}>
+            This is my content! This is my content!This is my content!This is my content!This is my content!This is my content!This is my content!This is my content!This is my content!This is my content!
+          </Grid>
+          <Grid item xs={0} sm={2}/>
+        </Grid>
+      </Grid>
+    </div>
+
+
+*/
+const App = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-        <h1>HELLO WORLD</h1>
-        <Button fullWidth="true" size="large" color="secondary" variant="contained">this is a button</Button>
+    <div className={classes.root}>
+      <CssBaseline/>
+      <Grid container direction="column">
+        <Grid item xs={12}>
+          <MenuBar></MenuBar>
+        </Grid>
+        <Grid item container>
+          <Grid item xs={0} sm={2}/>
+          <Grid item xs={12} sm={8}>
+            This is my content! This is my content!This is my content!This is my content!This is my content!This is my content!This is my content!This is my content!This is my content!This is my content!
+          </Grid>
+          <Grid item xs={0} sm={2}/>
+        </Grid>
+      </Grid>
     </div>
   );
-}
+  }
 
 export default App;
