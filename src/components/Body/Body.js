533f47e954caf798b43b40e95bef49d0c8e72aca
dkadillak <devin.kadillaka@gmail.com>
2021-02-08 10:16:45 -0700

    added body component, commiting to adddress ci/cd real quick

---
+++ src/components/Body/Body.js
@@ -0,0 +1,13 @@
+import { Typography } from '@material-ui/core';
+import React from 'react';
+
+
+const Body = (props) => {
+    return (
+        <Typography variant="body1">
+            This is my content! This is my content!This is my content!This is my content!This is my content!This is my content!This is my content!This is my content!This is my content!This is my content!
+        </Typography>
+    );
+};
+
+export default Body;
