5cd97ac03871866009cccc94de665673fa16f837
dkadillak <devin.kadillaka@gmail.com>
2021-02-09 16:45:40 -0700

    removing unused import

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { Grid, Typography, Paper } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 
 const Body = (props) => {
     return (
